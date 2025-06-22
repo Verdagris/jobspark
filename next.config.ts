d20a03af81d1b44e02e2b33bcf119305410882a4
Verdagris <75632225+Verdagris@users.noreply.github.com>
2025-06-22 19:15:12 +0200

    Fix Netlify image configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -37,7 +37,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'www.netlify.com',
         port: '',
-        pathname: '/**',
+        pathname: '/v3/img/components/**',
       },
     ],
   },
